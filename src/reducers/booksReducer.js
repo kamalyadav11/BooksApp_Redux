e04f73c7243af84a4ab87893ec3c676948e28fa7
kamal11 <kyzx11@gmail.com>
2018-09-11 23:33:30 +0530

    Initialized first reducer

---
+++ src/reducers/booksReducer.js
@@ -0,0 +1,9 @@
+export default () => {
+  return;
+  [
+    { title: "Harry Potter" },
+    { title: "JavaScript: The Good Parts" },
+    { title: "Eloquent JavaScript" },
+    { title: "The Tall Tower" }
+  ];
+};
