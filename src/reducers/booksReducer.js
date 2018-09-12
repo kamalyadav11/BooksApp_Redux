4b26ad0e9dacfd7d996716c762eb16f6979f9759
kamal11 <kyzx11@gmail.com>
2018-09-12 22:10:52 +0530

    finished SelectedBook Component

--- src/reducers/booksReducer.js
+++ src/reducers/booksReducer.js
@@ -1,8 +1,8 @@
 export default () => {
   return [
-    { title: "Harry Potter" },
-    { title: "JavaScript: The Good Parts" },
-    { title: "Eloquent JavaScript" },
-    { title: "The Tall Tower" }
+    { title: "Harry Potter", pages: 101 },
+    { title: "JavaScript: The Good Parts", pages: 87 },
+    { title: "Eloquent JavaScript", pages: 10 },
+    { title: "The Tall Tower", pages: 25 }
   ];
 };
